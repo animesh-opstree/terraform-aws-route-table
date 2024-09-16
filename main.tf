e5d78a330fe4f0f1891868e800eeffb7bb1d7399
animesh-opstree <animesh.yadav@opstree.com>
2024-09-16 10:51:47 +0530

    rt

--- main.tf
+++ main.tf
@@ -1,8 +1,12 @@
 resource "aws_route_table" "route_table" {
   vpc_id = var.vpc_id
-  route {
-    cidr_block = var.cidr
-    gateway_id = var.gateway_id
+
+  dynamic "route" {
+    for_each = (var.cidr != "" && var.gateway_id != "") ? [1] : []
+    content {
+      cidr_block = var.cidr
+      gateway_id = var.gateway_id
+    }
   }
 
   tags = merge(
